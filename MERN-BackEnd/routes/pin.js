526dafcdba15213ccea5458f97344aa7fa90bbe6
Aaron Everette <aeverette0120@gmail.com>
2022-10-16 12:53:45 -0400

    fixed conflicts

--- MERN-BackEnd/routes/pin.js
+++ MERN-BackEnd/routes/pin.js
@@ -1,10 +1,10 @@
 const router = require('express').Router();
-const pin = require('../models/pin');
+const Pin = require('../models/pin');
 
 //create a pin
 
 router.post("/", async (req,res)=>{
-    const newPin = new pin(req.body);
+    const newPin = new Pin(req.body);
     try{
         const savedPin = await newPin.save();
         res.status(200).json(savedPin);
@@ -17,10 +17,11 @@ router.post("/", async (req,res)=>{
 
 router.get("/", async (req,res)=>{
     try{
-        const pin = await pin.find();
+        const pin = await Pin.find();
         res.status(200).json(pin);
     }catch(err){
-        res.status(500).json(err)
+        console.log(err)
+        res.status(500).send('err')
     }
 })
 module.exports = router
