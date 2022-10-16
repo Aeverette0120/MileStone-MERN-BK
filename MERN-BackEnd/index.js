dd291da1906db790a99bbbba5cd2feb6e624a897
Aaron Everette <aeverette0120@gmail.com>
2022-10-16 12:34:32 -0400

    fixed conflicts

--- MERN-BackEnd/index.js
+++ MERN-BackEnd/index.js
@@ -1,13 +1,8 @@
 const express = require('express')
 const mongoose = require('mongoose')
-<<<<<<< HEAD
-const userRoutes = require('./controllers/user')
-const pinRoutes = require('./controllers/pin')
-=======
 // const userRoutes = require('./controllers/user')
 const pinRoutes = require('./routes/pin')
 
->>>>>>> 0aef4d3 (got pin route going)
 require('dotenv').config()
 
 const app = express()
@@ -16,13 +11,8 @@ const app = express()
 app.use(express.json())
 
 // routes
-<<<<<<< HEAD
-app.use('/user', userRoutes)
-app.use('/instrument', intrumentRoutes)
-=======
 // app.use('/user', userRoutes)
 app.use('/pin', pinRoutes)
->>>>>>> 0aef4d3 (got pin route going)
 
 // db connection
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true})
