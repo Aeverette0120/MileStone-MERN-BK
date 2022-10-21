f8a679edb424233f4cef4ba6b087c3d430381d1c
Aaron Everette <aeverette0120@gmail.com>
2022-10-20 20:41:21 -0400

    deleted default

--- models/pin.js
+++ models/pin.js
@@ -9,7 +9,6 @@ const pinSchema = new mongoose.Schema({
     },
     imgURL: {
         Type: String,
-        default:"https://images.unsplash.com/photo-1513002749550-c59d786b8e6c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
     },
     description: {
         type: String,
